ccaa9755f852f9c2acc3432e695c5e528e6d7a11
icanotc <44480394+icanotc@users.noreply.github.com>
2022-07-05 14:47:12 -0400

    svelteKit => svelte

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+
+  import {onMount} from "svelte"
+  import JitsiMeetJS from "@solyd/lib-jitsi-meet"
+
+
+  onMount(() => {
+    console.log(JitsiMeetJS)
+    JitsiMeetJS.init()
+  })
+
+
+
+</script>
+
+<main>
+  <div class="font-bold">
+    amogus
+  </div>
+</main>
+
+<style global>
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
+</style>
