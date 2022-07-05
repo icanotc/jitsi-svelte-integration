f432630daefa2dc8a566e977d442333e78231a52
Zeke-White <107807694+Zeke-White@users.noreply.github.com>
2022-07-05 15:19:46 -0400

    snek

--- src/App.svelte
+++ src/App.svelte
@@ -60,7 +60,11 @@
         </h1>
 
         <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={() => Mute_Unmute = !Mute_Unmute}>
-            {Mute_Unmute? "mute" : "unmute"}
+          {#if Mute_Unmute}  
+          <img src="snake.jpg"/>
+          {:else}
+          <img src="snakes.jpg"/>
+          {/if}
         </button>
     </div>
 </main>
