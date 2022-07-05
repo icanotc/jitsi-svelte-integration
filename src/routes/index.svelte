864bd669783eb3b44124132efe5dc9f242304719
Zeke-White <107807694+Zeke-White@users.noreply.github.com>
2022-07-05 14:36:23 -0400

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,15 @@
+
 <script context="module">
 	import {browser} from '$app/env'
 
-	import {JitsiMeetJS} from "../lib/vender/lib-jitsi-meet.min.js"
+	//import {JitsiMeetJS} from "../lib/vender/lib-jitsi-meet.min.js"
 
 
 	export const prerender = true;
 	
 </script>
+
+
 
 <script lang="ts">
 	const options = {
@@ -35,26 +38,72 @@
 		console.log("lmao")
 	}
 
-	function onConnectionSuccess() {
+	function onConnectionSuccess() 
+	{
 
 	}
-	function onConnectionFailed() {
+	function onConnectionFailed() 
+	{
 
 	}
-	function disconnect() {
+	function disconnect() 
+	{
 
 	}
+
+	function handleClick() 
+	{
+		alert('clicked')
+	}
+
+	var Mute_Unmute = true
+	let string = "Mute";
+
+	
+	
+
+
+	function handleClicks() {
+		Mute_Unmute = !Mute_Unmute
+
+		if (Mute_Unmute)
+		{
+			string = "Unmute";
+		}
+		else 
+		{
+			string = "Mute"	
+		}
+
+		
+
+	}
+
+	
+
 
 </script>
+
+
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
+
+
 <section>
 	<h1 class="text-3xl font-bold underline">
 		Hello world!
 	</h1>
+	<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={handleClick}>
+		Click me
+	</button>
+
+	<button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={handleClicks}>
+	{string}
+	</button> 
+
 
 </section>
