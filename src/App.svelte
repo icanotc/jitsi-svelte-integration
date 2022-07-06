44d529147bc53f69735bd56fba15327beacd9df7
icanotc <44480394+icanotc@users.noreply.github.com>
2022-07-06 11:16:15 -0400

    weird svelte issue fixed

--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,14 @@
 <script>
 
     import {onMount} from "svelte"
-    import "../src/lib/vender/lib-jitsi-meet.min.js"
+    import {InitJitsi} from "./lib/InitJitsi.js";
 
+    let MuteStatus = true
 
     onMount(() => {
-        console.log(JitsiMeetJS)
-        const DEFAULT_JITSI_CONFIG = {
-            hosts: {
-                domain: 'meet.jit.si',
-                muc: 'conference.meet.jit.si',
-                focus: 'focus.meet.jit.si',
-            },
-            externalConnectUrl: 'https://meet.jit.si/http-pre-bind',
-            useStunTurn: true,
-            bosh: `https://meet.jit.si/http-bind`, // need to add `room=[ROOM]` when joining
-            websocket: 'wss://meet.jit.si/xmpp-websocket',
-            clientNode: 'http://jitsi.org/jitsimeet',
-        }
-
-        const confOptions = {
-
-        };
-
-
-        JitsiMeetJS.init(confOptions)
-
-        let connection = new JitsiMeetJS.JitsiConnection(null, null, DEFAULT_JITSI_CONFIG);
-
-        connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
-        connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
-        connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
-
-        connection.connect();
-
-
-
-        function onConnectionSuccess()
-        {
-
-        }
-        function onConnectionFailed()
-        {
-
-        }
-        function disconnect()
-        {
-
-        }
-
-
+        InitJitsi()
     })
 
-    let Mute_Unmute = true
 
 </script>
 
@@ -62,8 +18,8 @@
             Hello world!
         </h1>
 
-        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={() => Mute_Unmute = !Mute_Unmute}>
-          {#if Mute_Unmute}  
+        <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={() => MuteStatus = !MuteStatus}>
+          {#if MuteStatus}
           <img src="snake.jpg"/>
           {:else}
           <img src="snakes.jpg"/>
