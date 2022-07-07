af2e1e57efe309ee6a82f7fb533d7a018cfa6f01
icanotc <44480394+icanotc@users.noreply.github.com>
2022-07-07 14:40:28 -0400

    fixed image upload, organized control bar components

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -1,15 +1,27 @@
 <script>
-    import {OverlayOn} from '../stores/OverlayStore.js' // two dots = diff folder  
-    import {bgImg} from '../stores/OverlayStore.js' // two dots = diff folder 
+    import { overlayEnabled } from "/src/stores/Overlay.js"
+    export let url, hidden
+
+
+    let image = url.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = e => {
+        url = e.target.result;
+    };
+
+
+
+
 </script>
 
 <div class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center">
 
     <div class="text-white rounded-md px-8 py-10 relative max-w-lg opacity-70 ">
         <!-- where prez goes  -->
-        <img src={$bgImg} class="border-8 border-black"> 
+        <img src={url} class="border-8 border-black">
         
-        <button class="absolute top-2 right-3 text4xl text-gray-300 hover:-translate-y-0.5 transtion-transform" on:click={() => OverlayOn.set(false)}>&times; </button>
+        <button class="absolute top-2 right-3 text4xl text-gray-300 hover:-translate-y-0.5 transtion-transform {hidden? 'hidden' : ''}" on:click={() => overlayEnabled.set(false)}>&times; </button>
     </div>
 
 </div>
--- src/stores/OverlayStore.js
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-
-export const OverlayOn = writable(false);
-export let bgImg = writable("");
