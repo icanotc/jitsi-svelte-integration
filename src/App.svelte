0e895aecec1208e078119f98c7878c0fe1b948fd
happiestbee <87628038+happiestbee@users.noreply.github.com>
2022-07-11 16:13:32 -0400

    moved pdf to separate file

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@ import './main.css'
 import { overlayEnabled, imgURL } from "./lib/stores/Overlay";
 import Overlay from "./lib/components/Overlay.svelte";
 import ControlBar from "./lib/components/ControlBar.svelte";
+import PdfCanvas from './lib/components/PdfCanvas.svelte';
 
 import {onMount} from "svelte"
 import {InitJitsi} from "./lib/js/InitJitsi";
@@ -12,44 +13,6 @@ import Video from "./lib/components/Video.svelte";
         //InitJitsi()
     })
 
-import pdfjs from "@bundled-es-modules/pdfjs-dist/build/pdf";
-import viewer from "@bundled-es-modules/pdfjs-dist/web/pdf_viewer";
-
-pdfjs.GlobalWorkerOptions.workerSrc =
-  "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.5.207/build/pdf.worker.min.js";
-var url = "./public/test.pdf";
-var loadingTask = pdfjs.getDocument(url);
-loadingTask.promise.then(function(pdf) {
-  console.log('PDF loaded');
-  
-  // Fetch the first page
-  var pageNumber = 1;
-  pdf.getPage(pageNumber).then(function(page) {
-    console.log('Page loaded');
-    
-    var scale = 1.5;
-    var viewport = page.getViewport({scale: scale});
-
-    // Prepare canvas using PDF page dimensions
-    var canvas = document.getElementById('the-canvas');
-    var context = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    // Render PDF page into canvas context
-    var renderContext = {
-      canvasContext: context,
-      viewport: viewport
-    };
-    var renderTask = page.render(renderContext);
-    renderTask.promise.then(function () {
-      console.log('Page rendered');
-    });
-  });
-}, function (reason) {
-  // PDF loading error
-  console.error(reason);
-});
 </script>
 
 
@@ -71,9 +34,8 @@ loadingTask.promise.then(function(pdf) {
 
     {/if}
     </div>
-    <div>
-        <canvas id="the-canvas"></canvas>
-    </div>
+    <PdfCanvas />
+    
 </main>
 
 <style global>
